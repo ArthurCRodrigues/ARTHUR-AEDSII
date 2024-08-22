22dfed3fc05e3b9b213294a8d7509af1156e52e0
Arthur <arodrigues@sga.pucminas.br>
2024-08-22 12:58:36 -0300

    Corrected special character problem

--- Practical-Assignments/PA1/CesaEncrypting.java
+++ Practical-Assignments/PA1/CesaEncrypting.java
@@ -1,34 +1,39 @@
 import java.util.Scanner;
 
 public class CesaEncrypting {
+
    public static int getCode(char x) {
       return (int) x; 
    }
+
    public static char getChar(int x) {
       return (char) x;
    }
+
    public static String encrypt(String wrd) {
       String newString = "";
-      for (int i = 0 ; i < wrd.length(); i++) {
+      for (int i = 0; i < wrd.length(); i++) {
          char x = wrd.charAt(i);
-         int code = getCode(x);
-         newString += getChar(code+3);
+
+         if (Character.isLetter(x) || Character.isDigit(x)) {
+            int code = getCode(x);
+            newString += getChar(code + 3);
+         } else {
+            newString += x; // Keep special characters unchanged
+         }
       }
       return newString;
-
    }
-   public static void main(String args[]) {
 
+   public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      
+
       String wrd = sc.nextLine();
       while (!wrd.equals("FIM")) {
-
          System.out.println(encrypt(wrd));
          wrd = sc.nextLine(); 
-        }
-        
-        sc.close();
-
+      }
+      
+      sc.close();
    }
 }
