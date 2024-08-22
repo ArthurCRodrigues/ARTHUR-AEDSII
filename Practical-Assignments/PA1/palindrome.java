2d67ea2e9b4ecb3ae9e113e79b35bf93d99dec52
ArthurSGA <arodrigues@sga.pucminas.br>
2024-08-21 21:56:55 -0300

    Java palindrome code for my Practical Assignment Fully implemented

---
+++ Practical-Assignments/PA1/palindrome.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class palindrome {
+    
+    public static int isPalindrome(String wrd) {
+        int lgt = wrd.length();
+        int lst_indx = lgt - 1;
+        for (int i = 0; i < lgt && i != lst_indx; i++) {
+            if (wrd.charAt(i) != wrd.charAt(lst_indx)) {
+                return 0;
+            }
+            lst_indx -= 1;
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String wrd = sc.nextLine();
+        
+        while (!wrd.equals("FIM")) {
+            String resp = isPalindrome(wrd) == 1 ? "SIM" : "NAO";
+            System.out.println(resp);
+            wrd = sc.nextLine(); 
+        }
+        
+        sc.close();
+    }
+}
