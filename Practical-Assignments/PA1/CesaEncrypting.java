971bfb82441d904ad58b7cbe9206b4a915547a7a
ArthurSGA <arodrigues@sga.pucminas.br>
2024-08-21 23:00:18 -0300

    Cesa Encrypting code fully implemented

---
+++ Practical-Assignments/PA1/CesaEncrypting.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class CesaEncrypting {
+   public static int getCode(char x) {
+      return (int) x; 
+   }
+   public static char getChar(int x) {
+      return (char) x;
+   }
+   public static String encrypt(String wrd) {
+      String newString = "";
+      for (int i = 0 ; i < wrd.length(); i++) {
+         char x = wrd.charAt(i);
+         int code = getCode(x);
+         newString += getChar(code+3);
+      }
+      return newString;
+
+   }
+   public static void main(String args[]) {
+
+      Scanner sc = new Scanner(System.in);
+      
+      String wrd = sc.nextLine();
+      while (!wrd.equals("FIM")) {
+
+         System.out.println(encrypt(wrd));
+         wrd = sc.nextLine(); 
+        }
+        
+        sc.close();
+
+   }
+}
