8dc25c32de56dc937ff390b9f4f2b332ed537f3e
ArthurSGA <arodrigues@sga.pucminas.br>
2024-08-25 19:37:07 -0300

    modified:   Practical-Assignments/PA1/RSR.c new file:   Practical-Assignments/PA1/output/RSR.exe

--- Practical-Assignments/PA1/RSR.c
+++ Practical-Assignments/PA1/RSR.c
@@ -1,1 +1,49 @@
-// Recursive Random Switch question in C
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+char getRandomChar() {
+    return 'a' + (rand() % 26);
+}
+
+void randomSwitch(char *wrd, char char1, char char2, int index) {
+
+    if (index == strlen(wrd)) {
+        return;
+    }
+
+
+    if (wrd[index] == char1) {
+        wrd[index] = char2;
+    }
+
+
+    randomSwitch(wrd, char1, char2, index + 1);
+}
+
+int main() {
+    srand(4); 
+    
+    char wrd[100];
+    scanf("%s", wrd);
+
+    while (strcmp(wrd, "FIM") != 0) {
+        char char1 = getRandomChar();
+        char char2;
+
+
+        do {
+            char2 = getRandomChar();
+        } while (char1 == char2);
+
+
+        randomSwitch(wrd, char1, char2, 0);
+
+        printf("%s\n", wrd);
+
+        scanf("%s", wrd);
+    }
+
+    return 0;
+}
