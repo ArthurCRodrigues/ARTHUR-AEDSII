be212b7517fd243c0a03185fb37239fd1d2f74ec
Arthur <arodrigues@sga.pucminas.br>
2024-08-26 09:40:54 -0300

    Recursive palindrome algorithm in Java

--- Practical-Assignments/PA1/RPJ.java
+++ Practical-Assignments/PA1/RPJ.java
@@ -3,24 +3,32 @@ public class RPJ {
     private static int isPalindromePrivate(String word,int head, int tail) {
         int len = word.length();
         int cmp = len - tail;
-        if (len == 1) {
+        System.out.println("HEAD -> "+head);
+        System.out.println("CMP -> "+cmp);
+
+        if (head == cmp) {
             return 0;
         } 
         if (word.charAt(head) != word.charAt(cmp)) {
             return 1;
         }
-        return isPalindromePrivate(word,head+1, tail -1);
+        else {
+            return isPalindromePrivate(word,head + 1, tail + 1);
+        }
 
     }
     public static void isPalindrome(String word) {
         if (isPalindromePrivate(word, 0, 1) == 0) {
             System.out.println("SIM");
+            return;
         }
-        if (isPalindromePrivate(word, 0, 0) == 1) {
+        if (isPalindromePrivate(word, 0, 1) == 1) {
             System.out.println("NAO");
+            return;
         }
         else {
             System.out.println("Unknown Error");
+            return;
         }
     }
     public static void main(String[] args) {
