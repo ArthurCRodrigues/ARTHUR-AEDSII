9196840b0411ba8c1f68a7db83a285756c6e874e
ArthurSGA <arodrigues@sga.pucminas.br>
2024-08-27 17:58:35 -0300

    recursiveCaesarCipher 100% working

---
+++ Practical-Assignments/PA1/recursiveCaesarCipher.java
@@ -0,0 +1,30 @@
+public class recursiveCaesarCipher {
+
+
+   private static String encrypt(String input, String newString,int index,int len) {
+        if (index == len) {
+            return newString;
+        }
+        char x = input.charAt(index);
+           
+        char ch = (char) (x + 3); // Adiciona o caractere criptografado à uma nova variavel
+           
+        newString += ch; // Adiciona o novo caractere à String
+
+        return encrypt(input, newString, index+1, len);
+   }
+
+   public static String encrypt(String input) {
+        return encrypt(input, "", 0,input.length());
+   }
+
+   public static void main(String[] args) {
+       String input = MyIO.readLine(); 
+
+       while (!input.equals("FIM")) {
+           String encryptedMessage = encrypt(input); 
+           MyIO.println(encryptedMessage); 
+           input = MyIO.readLine(); 
+       }
+   }
+}
