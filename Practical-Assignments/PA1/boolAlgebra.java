30f9faa1abc94a2646ebb79f7f8b451371088b24
ArthurSGA <arodrigues@sga.pucminas.br>
2024-08-30 09:35:15 -0300

    File Creation and tests

---
+++ Practical-Assignments/PA1/boolAlgebra.java
@@ -0,0 +1,8 @@
+public class boolAlgebra {
+    public static void main(String[] args) {
+        String input = "0 1 3";
+        int num = (int) input.charAt(0);
+        System.out.println(num+2);
+
+    }
+}
