d626fc066b0b031a1b9e9aef4baf22d417290188
ArthurSGA <arodrigues@sga.pucminas.br>
2024-09-02 20:58:04 -0300

    changes

---
+++ Practical-Assignments/PA1/output/production/ARTHUR-AEDSII/palindromeRecursive.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+int palRec(char *word , int tail) {
+    int length = strlen(word) - 1;
+    int indx = length - tail;
+    if (indx < 1) {
+        return 0;
+    }
+    if (word[0] != word[indx]) {
+        return 1;
+    }
+    else {
+        return palRec(&word[1],tail+1);
+    }
+}
+int palindromeRecursive( char *word) {
+    return palRec(word,1);
+}
+
+
+int main (void) { 
+    char str[255];
+
+    fgets(str, sizeof(str), stdin);
+
+    while (strcmp(str, "FIM\n") != 0) {
+        if (palindromeRecursive(str) == 0) {
+            printf("SIM\n");
+        } else {
+            printf("NAO\n");
+        }
+
+        fgets(str, sizeof(str), stdin);
+    }
+
+    return 0;
+}
