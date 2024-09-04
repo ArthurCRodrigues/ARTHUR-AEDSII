ee307fabf15236f4ba276ed87d188f7d62ea030b
Arthur <arodrigues@sga.pucminas.br>
2024-09-04 17:49:14 -0300

    Added new file sumCount.java and modified InsertionSort.java and SelectionSort.java

--- Sorting Algos/Selection Sort/InsertionSort.java
+++ Sorting Algos/Selection Sort/InsertionSort.java
@@ -1,5 +1,35 @@
+
+
 public class InsertionSort {
-    public static void Sort(int arr[]) {
+    public static int[] Sort(int arr[]) {
+        // Sorting algorithm using Selection Sort
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i]; // Store the current element
+            int j = i - 1; // Initialize the index of the previous element
+            
+            // Compare the current element with elements at indices
+            while ((j >= 0) && (arr[j] > temp)) {
+                arr[j + 1] = arr[j]; // Shift elements to the right
+                j--; // Move to the previous element
+            }
+            
+            arr[j + 1] = temp; // Insert the current element at the correct position
+        }
+        return arr; // Return the sorted array    
+    }
+    
+    public static void main(String[] args) {
+        int[] unsortedArray = {5, 2, 9, 1, 3};
+        System.out.println("Unsorted Array:");
+        for (int num : unsortedArray) {
+            System.out.print(num + " ");
+        }
         
+        int[] sortedArray = Sort(unsortedArray);
+        
+        System.out.println("\nSorted Array:");
+        for (int num : sortedArray) {
+            System.out.print(num + " ");
+        }
     }
 }
--- Sorting Algos/Selection Sort/SelectionSort.java
+++ Sorting Algos/Selection Sort/SelectionSort.java
@@ -1,35 +1,29 @@
-
-
 public class SelectionSort {
-    public static int[] Sort(int arr[]) {
-        // Sorting algorithm using Selection Sort
-        for (int i = 1; i < arr.length; i++) {
-            int temp = arr[i]; // Store the current element
-            int j = i - 1; // Initialize the index of the previous element
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
             
-            // Compare the current element with elements at indices
-            while ((j >= 0) && (arr[j] > temp)) {
-                arr[j + 1] = arr[j]; // Shift elements to the right
-                j--; // Move to the previous element
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
             }
             
-            arr[j + 1] = temp; // Insert the current element at the correct position
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
-        return arr; // Return the sorted array    
     }
     
     public static void main(String[] args) {
-        int[] unsortedArray = {5, 2, 9, 1, 3};
-        System.out.println("Unsorted Array:");
-        for (int num : unsortedArray) {
-            System.out.print(num + " ");
-        }
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
         
-        int[] sortedArray = Sort(unsortedArray);
-        
-        System.out.println("\nSorted Array:");
-        for (int num : sortedArray) {
-            System.out.print(num + " ");
+        System.out.println("Sorted array:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
