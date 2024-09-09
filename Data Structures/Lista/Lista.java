b9efa5ae25c382080c7a6872a08c1515e2718350
Arthur <arodrigues@sga.pucminas.br>
2024-09-09 15:36:17 -0300

    insertStart method implemented

--- Data Structures/Lista/Lista.java
+++ Data Structures/Lista/Lista.java
@@ -3,7 +3,7 @@
 public class Lista {
 
     public int[] array;
-    public int n;
+    public int n;// Next empty index
 
     public Lista() {
         this(6);
@@ -13,7 +13,21 @@ public class Lista {
         array = new int[tamanho];
         n = 0;
     }
-    
+    // Add an item to the first array slot, thus moving all the previous element to the right so that the new element can be placed.
+    public void insertStart(int x) throws Exception{
+        if (n >= array.length) {
+            throw new Exception("Error! Array index out of bounds");
+        }
+
+        for (int i = n ; i < 0 ; i++) {
+            array[i] = array[i-1]
+        }
+
+        array[0] = x;
+        n++;
+
+    }
+
 
 
 }
