362a6dede73e66e487ae9e2a8cd1e7e507332774
Arthur <arodrigues@sga.pucminas.br>
2024-09-09 11:37:22 -0300

    modified

--- Sorting Algos/BubbleSort/BubbleSort.java
+++ Sorting Algos/BubbleSort/BubbleSort.java
@@ -2,7 +2,8 @@ public class BubbleSort {
     public static void sort(int[] arr) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+            for (int j = 0; j < n - i - 1; j++) { 
+                // The - i in the for loop optimizes the algorithm to ignore the sorted elements of the array.
                 if (arr[j] > arr[j + 1]) {
                     // Swap arr[j] and arr[j+1]
                     int temp = arr[j];
