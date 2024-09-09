09ae65099b2b82ce3b613c45a509d574edbe9aeb
Arthur <arodrigues@sga.pucminas.br>
2024-09-09 15:29:02 -0300

    Started List class

---
+++ Practical-Assignments/PA1/Data Structures/Lista/Lista.java
@@ -0,0 +1,19 @@
+
+
+public class Lista {
+
+    public int[] array;
+    public int n;
+
+    public Lista() {
+        this(6);
+    }
+
+    public Lista(int tamanho) {
+        array = new int[tamanho];
+        n = 0;
+    }
+    
+
+
+}
