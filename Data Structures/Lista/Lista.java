f53e0e2d817c98d906a20aef2a161128278d8c8b
Arthur <arodrigues@sga.pucminas.br>
2024-09-10 18:21:23 -0300

    finished lista.java

--- Data Structures/Lista/Lista.java
+++ Data Structures/Lista/Lista.java
@@ -19,8 +19,8 @@ public class Lista {
             throw new Exception("Error! Array index out of bounds");
         }
 
-        for (int i = n ; i < 0 ; i++) {
-            array[i] = array[i-1]
+        for (int i = n ; i > 0 ; i--) {
+            array[i] = array[i-1];
         }
 
         array[0] = x;
@@ -37,10 +37,10 @@ public class Lista {
     }
 
     public void insert(int x , int pos) throws Exception {
-        if (n >= array.length) {
+        if (n >= array.length  || pos < 0 || pos > n) {
             throw new Exception("Error! Array index out of bounds");
         }
-        for (int i = n ; i > n; i--) {
+        for (int i = n ; i > pos; i--) {
             array[i] = array[i -1];
         }
 
@@ -73,17 +73,52 @@ public class Lista {
     }
 
     public int remove(int pos) throws Exception {
-        if (n == 0) {
+        if (n == 0 || pos < 0 || pos >= n) {
             throw new Exception("Error! Array is empty");
         }
         int resp = array[pos];
+        n--;
         
-        for (int i = n; i > pos; i--) {
-            array[i] = array[i-1];
+        for (int i = pos; i < n; i++) {
+            array[i] = array[i+1];
         }
-        
+        return resp;
     }
-
-
+    public void print() {
+        for (int i = 0 ; i < n ; i++) {
+            System.out.print(array[i]+" ");
+        }
+        System.out.print("\n");
+    }
+    public static void main(String[] args) {
+        try {
+            Lista lista = new Lista(6);
+            
+            lista.append(10);
+            lista.append(20);
+            lista.append(30);
+            lista.print(); // Output: 10 20 30
+            
+            lista.insertStart(5);
+            lista.print(); // Output: 5 10 20 30
+            
+            lista.insert(15, 2);
+            lista.print(); // Output: 5 10 15 20 30
+            
+            int removedItem = lista.removeStart();
+            System.out.println("Removed item: " + removedItem); // Output: Removed item: 5
+            lista.print(); // Output: 10 15 20 30
+            
+            int poppedItem = lista.pop();
+            System.out.println("Popped item: " + poppedItem); // Output: Popped item: 30
+            lista.print(); // Output: 10 15 20
+            
+            int removedItem2 = lista.remove(1);
+            System.out.println("Removed item: " + removedItem2); // Output: Removed item: 15
+            lista.print(); // Output: 10 20
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
+    }
 
 }
