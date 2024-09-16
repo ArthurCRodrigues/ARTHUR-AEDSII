bc8ebc2562d5f79826fad8ec3fd8ce0653e65812
Arthur <arodrigues@sga.pucminas.br>
2024-09-16 15:19:19 -0300

    santaSorting

--- Practical-Assignments/PEprep/santaSorting.c
+++ Practical-Assignments/PEprep/santaSorting.c
@@ -4,4 +4,16 @@
 int main ( void ) {
     char str[100][100];
     char temp[100];
+    int n;
+    scanf("%d",&n);
+    for (int i = 0 ; i < n ; i++) {
+        scanf("%s",&str[i]);
+    }
+    int well = 0;
+    for (int i = 0; i < n ; i++) {
+        if (str[i][0] == '+') {
+            well++;
+        }
+        
+    }
 }
