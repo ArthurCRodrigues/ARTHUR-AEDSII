afb9a3ed72f3deb17eabcb93d161e961d247b57e
Arthur <arodrigues@sga.pucminas.br>
2024-10-09 16:15:18 -0300

    Flexible stack completed

--- Data Structures/Flexible/Pilha/celula.java
+++ Data Structures/Flexible/Pilha/celula.java
@@ -1,4 +1,14 @@
 
-public class celula {
-    
+public class Celula {
+    public int elemento;
+    public Celula prox;
+    public Celula(){
+        this(0);
+    }
+    public Celula(int x) {
+        this.elemento = x;
+        this.prox = null;
+    }
+
+
 }
