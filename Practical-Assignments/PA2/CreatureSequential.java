7688f12fd65e50fa28f1fec01113e536d35db017
Arthur <arodrigues@sga.pucminas.br>
2024-10-10 23:51:26 -0300

    ongoing changes

--- Practical-Assignments/PA2/CreatureSequential.java
+++ Practical-Assignments/PA2/CreatureSequential.java
@@ -8,8 +8,36 @@ import java.util.List;
 import java.util.Scanner;
 
 class SequentialSearch {
+    List<Creature> allCreatures = 
+    public void GetAllCreatures() {
+        
+
+        try {
+            File file = new File("/tmp/pokemon.csv");
+            Scanner fileScanner = new Scanner(file);
+
+            if (fileScanner.hasNextLine()) {
+                fileScanner.nextLine();
+            }
+
+            while (fileScanner.hasNextLine()) {
+                String line = fileScanner.nextLine();
+                if (line.equals("FIM")) {
+                    break;
+                }
+
+                Creature creature = new Creature();
+                creature.readCreatureData(line);
+                creatures.add(creature);
+            }
+
+            fileScanner.close();
+        } catch (FileNotFoundException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
     public void addByID(int id) {
         // It should get the given parameter id and add the respective creature to the Creatures List.
-        
+        List<Creature> creatures
     }
 }
