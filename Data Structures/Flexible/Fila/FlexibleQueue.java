ea8b029c4324fe044ab7a6c9241b42daa436b9de
Arthur <arodrigues@sga.pucminas.br>
2024-10-29 09:39:20 -0300

    ex2 fila

--- Data Structures/Flexible/Fila/FlexibleQueue.java
+++ Data Structures/Flexible/Fila/FlexibleQueue.java
@@ -33,12 +33,24 @@ public class FlexibleQueue {
         System.out.print("]\n");
     }
 
+    public int maior() throws Exception {
+        if (primeiro == ultimo) throw new Exception("Erro!");
+        int maior = primeiro.prox.elemento;
+        for (Celula i = primeiro.prox; i != null; i = i.prox) {
+            if (i.elemento >= maior) {
+                maior = i.elemento;
+            }
+        }
+        return maior;
+    }
+
     public static void main(String[] args) {
         FlexibleQueue Fila = new FlexibleQueue();
         for (int i = 0; i < 10; i++) {
             Fila.inserir(i);
         }
         Fila.mostrar();
+        System.out.println("Maior elemento. -> "+ Fila.maior());
         for (int i = 0; i < 10; i++) {
             try {
                 System.out.println("Item removido! -> " + Fila.remover());
